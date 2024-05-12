411f4c249ecf0e5258cc878cc25760427e49a7f7
Dikshant Vyas <vyasdikshant1210@gmail.com>
2024-05-12 22:45:31 +0530

    start

---
+++ src/app/not-found.tsx
@@ -0,0 +1,8 @@
+'use client';
+
+import ErrorInformation from '@components/404/error-information';
+import './globals.css';
+
+export default function NotFound() {
+  return <ErrorInformation />;
+}
